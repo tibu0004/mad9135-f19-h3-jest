8767be7cca66f6e90bddf980dd4a14c2acb3893e
Joao Nabosne <tibu0004@algonquinlive.com>
2019-10-29 23:34:57 -0400

    added all the tests

--- robot.test.js
+++ robot.test.js
@@ -16,25 +16,44 @@ describe('Robot module', () => {
     // BUILD YOUR TESTS HERE
     
     // 1. Test that result is an array.
-
+    test('Test that result is an array', () => {
+      expect(Array.isArray(result))
+    });
 
     // 2. Test that result[1] is of type object.
+    test('Test that result[1] is of type object', () => {
+      expect(result[1] === Object)
+    });
 
+describe('Validate report values', () => {
+    // Create this variable after checking that type is object.
+    const report = result[1]; 
+    console.log(report.x);
+    
+    // 3. Test that report contains the properties: action, x, y, and facing.
+    test('Test that report contains the properties: action, x, y, and facing', () => {
+      expect(report).toHaveProperty('action','x','y','facing');
+    });
+      
+    // 4. Test that report.x is a number between 0 and 5.
+    test('Test that report.x is a number between 0 and 5', () => {
+      expect(report.x).toBeGreaterThanOrEqual(0);
+      expect(report.x).toBeLessThanOrEqual(5);
+    });
 
-    describe('Validate report values', () => {
-        // Create this variable after checking that type is object.
-        const report = result[1]; 
-
-        // 3. Test that report contains the properties: action, x, y, and facing.
-
-
-        // 4. Test that report.x is a number between 0 and 5.
-
-
-        // 5. Test that report.y is a number between 0 and 5.
-
+    // 5. Test that report.y is a number between 0 and 5.
+    test('Test that report.y is a number between 0 and 5', () => {
+      expect(report.y).toBeGreaterThanOrEqual(0);
+      expect(report.y).toBeLessThanOrEqual(5);
+    });
 
         // 6. Test that report.facing is one of: NORTH, SOUTH, EAST, or WEST.
+    test('Test that report.y is a number between 0 and 5', () => {
+      //expect(report.facing).toContain('NORTH','SOUTH','EAST','WEST');
+      expect(robot.cardinals).toContain(report.facing);
+    
+      
+    });
 
     })
 })
